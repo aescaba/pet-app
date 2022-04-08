9970e18c5b4982053f9236d99bda1a47626242a5
Amanda Escaba <amandaescaba@gmail.com>
2022-04-08 02:07:22 -0400

    added beginning of localstorage code to track visits

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, {useState} from 'react';
 
 function App() {
   const [isPet, setIsPet] = useState(false);
+  const []
   return (
     <div className="App">
       <header className="App-header">
@@ -29,4 +30,23 @@ function App() {
   );
 }
 
+// https://stackoverflow.com/questions/49945974/detect-re-visitors-using-localstorage
+function logVisit () {
+  // check if the localStorage object is supported by the browser
+  if ('localStorage' in window && window['localStorage'] !== null) {
+    var visits = localStorage.getItem('visits');
+    // if (visits == null)
+  }
+  return 
+}
+
 export default App;
+
+// refs
+// https://stackoverflow.com/questions/61054275/usestate-with-boolean-value-in-react
+// https://dommagnifi.co/2020-12-03-toggle-state-with-react-hooks/
+// https://dev.to/alexkhismatulin/update-boolean-state-right-with-react-hooks-3k2i
+// https://felixgerschau.com/react-localstorage/
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+// https://stackoverflow.com/questions/49945974/detect-re-visitors-using-localstorage
+// https://developer.mozilla.org/en-US/docs/Web/API/Storage/setItem
