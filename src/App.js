60d159b598e9eeff74f39e1f76bb21856ee1deaf
Amanda Escaba <amandaescaba@gmail.com>
2022-04-08 01:38:25 -0400

    edit metadata and test react hook state changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [isPet, setIsPet] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          hmmm
-        </p>
+        <h1>
+          did you pet franklin today?
+        </h1>
+        <p>{String(isPet)}</p>
+        <button onClick={() => setIsPet(!isPet)}>
+          pet
+        </button>
       </header>
+      <div>
+
+      </div>
+    {/* <div>
+      <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>
+         Click me
+      </button>
+  </div>*/}
+
     </div>
   );
 }
